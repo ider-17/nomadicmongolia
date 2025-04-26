1c7d35afccc605fcdf1ea26652cfb575ab77bf58
ider-17 <ider7037@gmail.com>
2025-04-26 21:55:36 +0800

    updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,19 @@
 "use client";
 
 import { Header } from "@/components/Header";
-import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
 
   const [currentBackground, setCurrentBackground] = useState(0);
 
-  const backgrounds = ["/_ZAK4316.jpg", "/_BAY5051.jpg", "/_BAY5051.jpg", "/_BAY1429.jpg", "/_BAY0058.jpg"];
+  const backgrounds = ["/_ZAK4316.jpg", "/_BAY5051.jpg", "/_BAY1429.jpg", "/_BAY0058.jpg"];
 
   const changeBackground = () => {
-    setCurrentBackground((prevIndex) => (prevIndex + 1) % backgrounds.length);
+    setCurrentBackground(currentBackground + 1);
+    if (currentBackground === 3) {
+      setCurrentBackground(0)
+    }
   }
 
   return (
