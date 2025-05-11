ad78573ae0d26f1d5e6853daf6b018a33feb2fef
ider-17 <ider7037@gmail.com>
2025-05-11 22:44:47 +0800

    nothing

--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -1,9 +1,14 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 
 export default function ToursPage() {
     return (
         <div className="bg-white h-screen">
+
             <Header />
+
+            <Footer />
+
         </div>
     )
 };
