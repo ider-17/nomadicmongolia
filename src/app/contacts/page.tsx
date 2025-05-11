d13f5f06df77312a0ea7035eefbb6f65c250e9b0
ider-17 <ider7037@gmail.com>
2025-05-11 22:42:09 +0800

    updated footer added

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -1,9 +1,12 @@
+import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
 
 export default function ContactPage() {
     return (
         <div className="bg-white h-screen">
             <Header />
+
+            <Footer />
 
         </div>
     )
