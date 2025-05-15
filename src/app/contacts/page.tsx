6325526d41083ce1d7cea7b095f43ff2d028ceb3
ider-17 <ider7037@gmail.com>
2025-05-15 21:49:21 +0800

    updasjfk

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -12,3 +12,80 @@ export default function ContactPage() {
         </div>
     )
 };
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
+
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
+
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
--- src/app/tours/page.tsx
+++ src/app/tours/page.tsx
@@ -12,3 +12,51 @@ export default function ToursPage() {
         </div>
     )
 };
+
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
+
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
+
+
+// import { Footer } from "@/components/Footer";
+// import { Header } from "@/components/Header";
+
+// export default function ContactPage() {
+//     return (
+//         <div className="bg-white h-screen">
+
+//             <Header />
+
+//             <Footer />
+
+//         </div>
+//     )
+// };
