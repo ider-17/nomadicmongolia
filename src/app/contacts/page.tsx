e95ef5479d1a69abb8eb5719bcdbc88af4a8212c
ider-17 <ider7037@gmail.com>
2025-05-15 21:48:22 +0800

    akljd

--- src/app/contacts/page.tsx
+++ src/app/contacts/page.tsx
@@ -12,35 +12,3 @@ export default function ContactPage() {
         </div>
     )
 };
-
-
-// import { Footer } from "@/components/Footer";
-// import { Header } from "@/components/Header";
-
-// export default function ContactPage() {
-//     return (
-//         <div className="bg-white h-screen">
-
-//             <Header />
-
-//             <Footer />
-
-//         </div>
-//     )
-// };
-
-
-// import { Footer } from "@/components/Footer";
-// import { Header } from "@/components/Header";
-
-// export default function ContactPage() {
-//     return (
-//         <div className="bg-white h-screen">
-
-//             <Header />
-
-//             <Footer />
-
-//         </div>
-//     )
-// };
