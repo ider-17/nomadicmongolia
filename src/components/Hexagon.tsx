b94f5f78ff664a55e58af10ce9e339471e4e78e4
ider-17 <ider7037@gmail.com>
2025-05-19 21:22:28 +0800

    updated

--- src/components/Hexagon.tsx
+++ src/components/Hexagon.tsx
@@ -1,23 +1,27 @@
-import { Check } from "lucide-react";
+import { Check, Clock3, Handshake, MapPinned, ShieldCheck, Star } from "lucide-react";
 
 const Hexagon = () => {
     return (
         <div className="relative w-full h-[500px]">
             <div className="absolute top-[30px] left-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex flex-col justify-center items-center">
-                <Check size={50} color="#2c2b2b" />
-                <p>Siamo responsabili</p>
+                <ShieldCheck size={50} color="#2c2b2b" />
+                <p className="text-[#2c2b2b]">Siamo responsabili</p>
             </div>
-            <div className="absolute top-[30px] right-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex justify-center items-center">
+            <div className="absolute top-[30px] right-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex flex-col justify-center items-center">
+                <Star size={50} />
                 <p>Siamo esperti</p>
             </div>
-            <div className="absolute top-1/2 left-1/2 -translate-1/2 w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex justify-center items-center">
+            <div className="absolute top-1/2 left-1/2 -translate-1/2 w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex flex-col justify-center items-center">
+                <MapPinned size={50} />
                 <p>Siamo locali</p>
             </div>
-            <div className="absolute bottom-[30px] left-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex justify-center items-center">
+            <div className="absolute bottom-[30px] left-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex flex-col justify-center items-center">
+                <Handshake size={50} />
                 <p>Siamo ospitali</p>
             </div>
-            <div className="absolute bottom-[30px] right-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex justify-center items-center">
-                <p>Siamo sempre a disposizione</p>
+            <div className="absolute bottom-[30px] right-[250px] w-[250px] h-[220px] hexagon bg-[#e4e8f0] mx-auto flex flex-col justify-center items-center">
+                <Clock3 size={50} />
+                <p className="text-center">Siamo sempre a <br /> disposizione</p>
             </div>
         </div>
     );
