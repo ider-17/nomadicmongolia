b6db45a3d3b3564bb204053721868d7b3acbe167
ider-17 <ider7037@gmail.com>
2026-01-14 21:09:39 +0800

    removed runtime

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -12,8 +12,6 @@ import config from "../../../../sanity.config";
 
 export const dynamic = "force-static";
 
-export const runtime = "nodejs";
-
 export { metadata, viewport } from "next-sanity/studio";
 
 export default function StudioPage() {
